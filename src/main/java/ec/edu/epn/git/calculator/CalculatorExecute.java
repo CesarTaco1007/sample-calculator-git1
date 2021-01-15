f02d1fcd0313e0182cdf0307e1dd98c420c5952a
ctaco <cesar.taco@epn.edu.ec>
2021-01-15 12:16:18 -0500

    Initial commit

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,15 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+        System.out.println("== CALCULATOR EXECUTE ==");
+
+        Calculator c = new Calculator();
+        int add = c.addition(4,7);
+        System.out.println("c.addition(4,7)" + add);
+
+        int sus = c.subtraction(7,1);
+        System.out.println("c.subtraction(7,1)" + sus);
+
+    }
+}
